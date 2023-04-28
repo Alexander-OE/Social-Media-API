4ca445dbb07a87dd89911f9dba4cf7db539b818f
Egbe Alexander <alexsucceeds@gmail.com>
2023-04-28 01:32:21 +0100

    work on user update route

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,34 @@
-const router = require("express").Router()
+const router = require("express").Router();
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
 // update user
 router.put("/:id", async (req, res) => {
-    if(req.body.userId === req.params.id || req.user.isAdmin){
-
-    }else{
-        return res.status(403)
+  if (req.body.userId === req.params.id || req.user.isAdmin) {
+    if (req.body.password) {
+      try {
+        const salt = await bcrypt.genSalt(10);
+        req.body.password = await bcrypt.hash(req.body.password, salt);
+      } catch (err) {
+        return res.status(500).json(err);
+      }
     }
-})
+    try {
+      const user = await User.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: req.body,
+        },
+        { new: true }
+      );
+      res.status(200).json("Account has been updated")
+    } catch (error) {
+        return res.status(500).json(err);
+    }
+  } else {
+    return res.status(403).json("you can update only your account");
+  }
+});
 // delete user
 //get a user
 // follow a user
