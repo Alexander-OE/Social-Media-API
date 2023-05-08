656bd6e63c127c74ad1340705c97f57a9b154e73
Egbe Alexander <alexsucceeds@gmail.com>
2023-05-08 21:44:44 +0100

    worked on update post feature

--- routes/post.js
+++ routes/post.js
@@ -12,3 +12,19 @@ router.post("/", async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+// update a post
+router.put("/:id", async (req, res) => {
+  const post = Post.findById(req.params.id);
+  try {
+    if (post.userId === req.body.userId) {
+      await post.updateOne({$set: req.body});
+      res.status(200).json("the post has been updated")
+
+    } else {
+      res.status(403).json("you can update only your post");
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
