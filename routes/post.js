6a579650d57c0c641c5dacf42bb8d99be9e9481f
Egbe Alexander <alexsucceeds@gmail.com>
2023-05-09 17:04:23 +0100

    worked on the like $ dislike endpoint

--- routes/post.js
+++ routes/post.js
@@ -18,13 +18,27 @@ router.put("/:id", async (req, res) => {
   const post = Post.findById(req.params.id);
   try {
     if (post.userId === req.body.userId) {
-      await post.updateOne({$set: req.body});
-      res.status(200).json("the post has been updated")
-
+      await post.updateOne({ $set: req.body });
+      res.status(200).json("the post has been updated");
     } else {
       res.status(403).json("you can update only your post");
     }
   } catch (error) {
     res.status(500).json(error);
   }
+});
+
+// like / dislike a post
+
+router.put("/:id/like", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post.likes.includes(req.body.userId)) {
+      await post.updateOne({ $push: { likes: req.body.userId } });
+      res.status(200).json("The post has been liked");
+    } else {
+      await post.updateOne({ $pull: { likes: req.body.userId } });
+      res.status(200).json("The post has been disliked");
+    }
+  } catch (error) {}
 });
