369d28b2b68120225fd427ad038b841b10420f1e
Egbe Alexander <alexsucceeds@gmail.com>
2023-05-11 16:02:19 +0100

    worked on get timeline post

--- routes/post.js
+++ routes/post.js
@@ -53,3 +53,20 @@ router.get("/:id", async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+// get timeline posts
+
+router.get("/timeline/all", async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.body.userID);
+    const userPosts = await Post.find({ userId: currentUser._id });
+    const friendPosts = await Promise.all(
+      currentUser.followings.map((friendId) => {
+       return  Post.find({ userId: friendId });
+      })
+    );
+    res.json(userPosts.concat(...friendPosts));
+  } catch (error) {
+    res.status(500).json(err);
+  }
+});
